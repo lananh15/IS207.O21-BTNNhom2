f1079697947d72b05e886ae8769c353cf4d1a6ee
lananh15 <lananhngo685@gmail.com>
2024-04-08 19:52:42 +0700

    push code lan 2

--- calculator.js
+++ calculator.js
@@ -1,51 +1,63 @@
-var so=document.getElementsByClassName("so");
-var dongpheptinh=document.getElementById("pheptinh");
-var ketqua=document.getElementById("ketqua");
-
-for(var i=0;i<so.length;i++){
+let so=document.getElementsByClassName("so");
+let dongpheptinh=document.getElementById("pheptinh");
+let ketqua=document.getElementById("ketqua");
+function checkResultAndDisplay(x){
+    if(ketqua.textContent!=''){
+        dongpheptinh.textContent='';
+        ketqua.textContent='';
+        dongpheptinh.textContent+=x;      
+    }
+    else{
+        dongpheptinh.textContent+=x;
+    }
+}
+// dinh nghia cho nut so
+for(let i=0;i<so.length;i++){
     so[i].addEventListener("click",function(){
-        var soduocclick=this.textContent;
-        dongpheptinh.innerHTML+=soduocclick;
+        let soduocclick=this.textContent;
+        checkResultAndDisplay(soduocclick);
     })
 }
 // dinh nghia cho nut CE
-var ce=document.getElementById("CE-btn");
+const ce=document.getElementById("CE-btn");
 ce.addEventListener("click",function(){
     dongpheptinh.textContent='';
     ketqua.textContent='';
 })
 // dinh nghia cho nut DEL
-var del=document.getElementById("Del-btn");
+const del=document.getElementById("Del-btn");
 del.addEventListener("click",function(){
-    var currentDongpheptinh= dongpheptinh.textContent;
+    let currentDongpheptinh= dongpheptinh.textContent;
     dongpheptinh.textContent=currentDongpheptinh.slice(0,-1);
     ketqua.textContent='';
 })
 // dinh nghia hien thi cho day phay
-var dauphay=document.getElementById("dauphay");
+const dauphay=document.getElementById("dauphay");
 dauphay.addEventListener("click",function(){
-    dongpheptinh.textContent+=".";
+    let x=".";
+    checkResultAndDisplay(x);
 })
 // dinh nghia hien thi cac dau phep tinh
-var dau=document.getElementsByClassName("daupheptinh");
-for (var i=0;i<dau.length;i++){
+let dau=document.getElementsByClassName("daupheptinh");
+for (let i=0;i<dau.length;i++){
     dau[i].addEventListener("click",function(){
-        var dauduocclick=this.textContent;
-        dongpheptinh.textContent+=dauduocclick;
+        let dauduocclick=this.textContent;
+        checkResultAndDisplay(dauduocclick);
     })
 }
 // dinh nghia cho dau bang
-var daubang=document.getElementById("daubang");
+const daubang=document.getElementById("daubang");
 daubang.addEventListener("click",function(){
-    if(!/\d/.test(dongpheptinh.textContent) || /\D{2}/.test(dongpheptinh.textContent)){
+    // neu phep tinh ko co chu so nao hoac co chua 2 dau phep tinh ke nhau thi bao loi
+    let a=dongpheptinh.textContent;
+    if(!/\d/.test(a) || /\D{2}/.test(a) || a.includes("/0")){
         dongpheptinh.textContent="Error";
         setTimeout(function(){
             dongpheptinh.textContent = '';
         },400);
     }
-    else{
-        var pheptinh=dongpheptinh.textContent;
-        var result=math.evaluate(pheptinh);
-        ketqua.textContent=Math.round(result * 10000000000) / 10000000000;
-    }
+    let pheptinh=dongpheptinh.textContent;
+    let result=math.evaluate(pheptinh);
+    let x=Math.round(result* 10000000000) / 10000000000;
+    ketqua.textContent=x;
 });
